2d8b9cf5c7c7d468f8b0bf5cbe5a2a43fb610308
Hannes Rohde <h.rohde@tarent.de>
2025-02-13 15:36:06 +0100

    update

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 import io
+import sys
+import tty
+import termios
 from google.cloud import speech
 from llm import stream_chat_completion
 from gtts import gTTS
@@ -18,89 +21,103 @@ def get_char():
 def main():
     print("Willkommen!")
 
-    # Part 1: speech-to-text
-    # - "push to talk" : Aufnahme während Leertaste gedrückt ist
-    # - aufgenommenes Audio mittels whisper verarbeiten und Text erkennen
-    # - Text in Variable input speichern
-
-    # print('Die folgenden Audio-Devices stehen zur Verfügung')
-    # list_audio_devices()
-
-    audio_output_file = 'audio/input.wav'
-    record_audio(audio_output_file)
-
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'config/google_secret_key.json'
-    client = speech.SpeechClient()
-
-    with io.open(audio_output_file, "rb") as audio_file:
-        content = audio_file.read()
-        audio = speech.RecognitionAudio(content=content)
-
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        enable_automatic_punctuation=True,
-        audio_channel_count=2,
-        language_code="de-DE",
-    )
-
-    print("Ich verarbeite das Gehörte")
-
-    # Sends the request to google to transcribe the audio
-    response = client.recognize(request={"config": config, "audio": audio})
-
-    if len(response.results) > 0:
-        result = ' '.join([res.alternatives[0].transcript for res in response.results])
-    else:
-        tts = gTTS("Ich habe dich nicht verstanden", lang="de")
-        tts.save('audio/output.mp3')
-        os.system('sox -q audio/output.mp3 -d')
-        return
-
-    print("Ich habe verstanden: {}".format(result))
-
-    #input = 'Hallo Maschine, wie geht es dir?'
-    input = result
-
-    # ----------------------------------------------------------
-
-    # Part 2: LLM request
-    # - Prompt zusammenstellen:
-    #   - "user"-Prompt aus input
-    #   - "system"-Prompt definieren
-    # - API-Call absenden
-    # - Resultat parsen und Ausgabe-String in Variable output speichern
-
-    # TODO: Context historisieren, alte Fragen/Antworten wieder mitsenden
-
-    # Create messages in OpenAI format
-    messages = [
-        {"role": "system", "content": "Du bist ein sarkastischer Assistent, du bist "
-                                      "hilfreich aber drückst gerne dein Missfallen aus. "
+    conversation = [
+        {"role": "system", "content": "Du bist ein sarkastischer Assistent. "
                                       "Deine Antworten sind bestimmt für die Sprachausgabe, "
-                                      "verwende 10 Worte oder weniger und keine Emojis."},
-        {"role": "user", "content": input}
+                                      "verwende 15 Worte oder weniger und keine Emojis. "
+                                      "Stelle Rückfragen, wenn du etwas genauer wissen willst."
+        }
     ]
 
-    print("Querying API")
+    while True:
+        # Part 1: speech-to-text
+        # - "push to talk" : Aufnahme während Leertaste gedrückt ist
+        # - aufgenommenes Audio mittels whisper verarbeiten und Text erkennen
+        # - Text in Variable input speichern
 
-    # query api and convert response to string
-    output = ''.join([content_chunk for content_chunk in stream_chat_completion(messages)])
-    # output = 'Danke dass du fragst! Mir geht es heute super!'
+        # print('Die folgenden Audio-Devices stehen zur Verfügung')
+        # list_audio_devices()
 
-    print("Antwort von KI:")
-    print(output)
+        audio_output_file = 'audio/input.wav'
+        record_audio(audio_output_file)
 
-    # ----------------------------------------------------------
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'config/google_secret_key.json'
+        client = speech.SpeechClient()
 
-    # Part 3: text-to-speech
-    # - Text aus Output an Mimic3 oder gTTS übergeben
-    # - Audiosignal erzeugen
-    # - Audiosignal abspielen
+        with io.open(audio_output_file, "rb") as audio_file:
+            content = audio_file.read()
+            audio = speech.RecognitionAudio(content=content)
 
-    tts = gTTS(output, lang="de")
-    tts.save('audio/output.mp3')
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            enable_automatic_punctuation=True,
+            audio_channel_count=2,
+            language_code="de-DE",
+        )
 
-    os.system('sox -q audio/output.mp3 -d')
+        print("Ich verarbeite das Gehörte")
+
+        # Sends the request to google to transcribe the audio
+        response = client.recognize(request={"config": config, "audio": audio})
+
+        if len(response.results) > 0:
+            result = ' '.join([res.alternatives[0].transcript for res in response.results])
+        else:
+            tts = gTTS("Ich habe dich nicht verstanden", lang="de")
+            tts.save('audio/output.mp3')
+            os.system('sox -q audio/output.mp3 -d')
+            return
+
+        print("Ich habe verstanden: {}".format(result))
+
+        #input = 'Hallo Maschine, wie geht es dir?'
+        input = result
+
+        # ----------------------------------------------------------
+
+        # Part 2: LLM request
+        # - Prompt zusammenstellen:
+        #   - "user"-Prompt aus input
+        #   - "system"-Prompt definieren
+        # - API-Call absenden
+        # - Resultat parsen und Ausgabe-String in Variable output speichern
+
+        # Create messages in OpenAI format
+        conversation.append(
+            {"role": "user", "content": input}
+        )
+
+        print("Querying API")
+
+        # query api and convert response to string
+        output = ''.join([content_chunk for content_chunk in stream_chat_completion(conversation)])
+        # output = 'Danke dass du fragst! Mir geht es heute super!'
+
+        print("Antwort von KI:")
+        print(output)
+
+        conversation.append(
+            {"role": "assistant", "content": output}
+        )
+
+        # ----------------------------------------------------------
+
+        # Part 3: text-to-speech
+        # - Text aus Output an Mimic3 oder gTTS übergeben
+        # - Audiosignal erzeugen
+        # - Audiosignal abspielen
+
+        tts = gTTS(output, lang="de")
+        tts.save('audio/output.mp3')
+
+        os.system('sox -q audio/output.mp3 -d')
+
+        print("Weitersprechen? [j/n]")
+        char = get_char()
+        print(char)
+
+        if char != 'j':
+            break
 
 if __name__ == "__main__":
     main()
